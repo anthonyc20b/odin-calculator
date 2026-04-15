31107df7c31ab0204043c9ae22567aec6bdefbc7
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-15 09:33:43 -0600

    Create base level functions for math operators
    Included with console.log tests to ensure that each function is
    working as designed.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("This is a test");
+const operationAdd = function (a, b) {
+ return a + b;
+}
+console.log(operationAdd(2, 3));
+
+const operationSubtract = function (a, b){
+    return a - b;
+}
+console.log(operationSubtract(6, 5));
+
+const operationMultiply = function (a, b){
+    return a * b;
+}
+console.log(operationMultiply(3, 5))
+
+const operationDivide = function (a, b){
+    return a / b;
+}
+console.log(operationDivide(25, 5));
