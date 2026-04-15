4fbef8968f9beef1d537a47da04477e7318ae84c
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-15 09:45:04 -0600

    Add function that completes math operation
    Also remove all tests from project files; Next step creating
    the calculator UI

--- javascript.js
+++ javascript.js
@@ -1,19 +1,27 @@
 const operationAdd = function (a, b) {
  return a + b;
 }
-console.log(operationAdd(2, 3));
 
 const operationSubtract = function (a, b){
     return a - b;
 }
-console.log(operationSubtract(6, 5));
 
 const operationMultiply = function (a, b){
     return a * b;
 }
-console.log(operationMultiply(3, 5))
 
 const operationDivide = function (a, b){
     return a / b;
 }
-console.log(operationDivide(25, 5));
+
+// Go back and update to actually run off of user input
+let userNumberOne = 3;
+let userOperator = "";
+let userNumberTwo = 4;
+
+const operate = function (){
+    let a = userNumberOne;
+    let b = userNumberTwo;
+    return operationAdd(a, b);
+};
+console.log(operate());
