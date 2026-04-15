65c96c926a890be5445c7b1c56c85c6907e225c1
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-15 10:29:58 -0600

    Add updating display based on selected number
    Function updates the first number variable upon user selection.
    As well as creates an update to the calculator display to show current
    number selection.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,39 @@
-const operationAdd = function (a, b) {
- return a + b;
-}
+const operationAdd = function (a, b) {return a + b;};
+const operationSubtract = function (a, b){return a - b;};
+const operationMultiply = function (a, b){return a * b;};
+const operationDivide = function (a, b){return a / b;};
 
-const operationSubtract = function (a, b){
-    return a - b;
-}
+const numberDisplay = document.querySelector("#current-number");
 
-const operationMultiply = function (a, b){
-    return a * b;
-}
+const userNumberOne = document.querySelectorAll(".btn-number");
+    for (let button of userNumberOne){
+        button.addEventListener("click", () => {
+            const numberChoice = button.textContent
+            console.log("The first number is:", numberChoice);
+            numberDisplay.textContent = numberChoice;
+        })
+    }
 
-const operationDivide = function (a, b){
-    return a / b;
-}
+const userOperator = "";
 
-// Go back and update to actually run off of user input
-let userNumberOne = 3;
-let userOperator = "";
+
 let userNumberTwo = 4;
 
 const operate = function (){
     let a = userNumberOne;
     let b = userNumberTwo;
-    return operationAdd(a, b);
+
+    if (userOperator === "+"){
+        return operationAdd(a, b);
+    } else if (userOperator === "-") {
+        return operationSubtract(a, b);
+    } else if (userOperator === "*") {
+        return operationMultiply(a, b);
+    } else if (userOperator === "/") {
+        return operationDivide(a, b);
+    } else {
+        return "Error: Invalid Selection";
+    }
 };
+// Used for testing the function in the console
 console.log(operate());
