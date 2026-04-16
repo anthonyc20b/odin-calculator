4bd4c79ad6517e353841e7b53ff006fad5cef8e1
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-16 11:54:52 -0600

    Add in equals event listener and final calculation
    The program now waits to run until the final equals operator is
    selected. Once selected it then runs the calculation with the user
    selected operator, number one, and number two. Then updates the DOM
    to display the final answer.

--- javascript.js
+++ javascript.js
@@ -8,13 +8,14 @@ let firstDisplayNumber = ""; // Used to show the current state of the calculator
 let secondDisplayNumber = ""; // Used to show the current state of the calculator, updates the DOM for number 2
 let stringConvert = 0; // For function purposes this is a
 let stringConvertTwo = 0; // For function purposes this is b
-let operatorChosen = false;
+let operatorChosen = false; // Used to conditionally check if we are selecting number one or number two
+let operatorSelect = "";
 
 const userNumberOne = document.querySelectorAll(".btn-number");
     for (let button of userNumberOne){
         button.addEventListener("click", () => {
             if (operatorChosen === false){
-                const numberSelect = button.textContent;
+                let numberSelect = button.textContent;
                 firstDisplayNumber = firstDisplayNumber + numberSelect;
                 numberDisplay.textContent = firstDisplayNumber;
                 stringConvert = parseInt(firstDisplayNumber)
@@ -25,41 +26,46 @@ const userNumberOne = document.querySelectorAll(".btn-number");
 const userOperator = document.querySelectorAll(".btn-operator");
     for (let button of userOperator){
         button.addEventListener("click", () => {
-            const operatorSelect = button.textContent;
+            operatorSelect = button.textContent;
             console.log("The operator selection is:", operatorSelect);
             numberDisplay.textContent = operatorSelect;
             return operatorChosen = true;
+            return operatorSelect;
         })
     }
 
-
-let userNumberTwo = document.querySelectorAll(".btn-number");
+const userNumberTwo = document.querySelectorAll(".btn-number");
     for (let button of userNumberTwo) {
         button.addEventListener("click", () => {
             if (operatorChosen === true){
-                const numberSelect = button.textContent;
+                let numberSelect = button.textContent;
                 secondDisplayNumber = secondDisplayNumber + numberSelect;
                 numberDisplay.textContent = secondDisplayNumber;
                 stringConvertTwo = parseInt(secondDisplayNumber)
-                console.log("The first number is:", secondDisplayNumber);
+                console.log("The second number is:", secondDisplayNumber);
         }})
     }
 
-const operate = function (){
-    let a = userNumberOne;
-    let b = userNumberTwo;
+const finalCalculation = document.querySelector("#btn-equals");
+finalCalculation.addEventListener("click", () => {
+    const operate = function (){
+        let a = stringConvert;
+        let b = stringConvertTwo;
 
-    if (userOperator === "+"){
-        return operationAdd(a, b);
-    } else if (userOperator === "-") {
-        return operationSubtract(a, b);
-    } else if (userOperator === "*") {
-        return operationMultiply(a, b);
-    } else if (userOperator === "/") {
-        return operationDivide(a, b);
-    } else {
-        return "Error: Invalid Selection";
-    }
-};
-// Used for testing the function in the console
-console.log(operate());
+        if (operatorSelect === "+"){
+            return operationAdd(a, b);
+        } else if (operatorSelect === "-") {
+            return operationSubtract(a, b);
+        } else if (operatorSelect === "*") {
+            return operationMultiply(a, b);
+        } else if (operatorSelect === "/") {
+            return operationDivide(a, b);
+        } else {
+            return "Error: Invalid Selection";
+        }
+    };
+    // Used for testing the function in the console
+    let finalResult = operate();
+    console.log("The final result is:", finalResult);
+    numberDisplay.textContent = finalResult;
+})
