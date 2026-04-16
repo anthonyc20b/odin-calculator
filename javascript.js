e54f2252aeb3672a4712285bfaddc55c556e512a
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-16 12:10:58 -0600

    Update display to show each operation added
    Display now includes each number and operator added before the final
    calculation is made.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
+// Basic functions included for mathematical logic
 const operationAdd = function (a, b){return a + b;};
 const operationSubtract = function (a, b){return a - b;};
 const operationMultiply = function (a, b){return a * b;};
 const operationDivide = function (a, b){return a / b;};
 
-const numberDisplay = document.querySelector("#current-number");
 let firstDisplayNumber = ""; // Used to show the current state of the calculator, updates the DOM for number 1
 let secondDisplayNumber = ""; // Used to show the current state of the calculator, updates the DOM for number 2
 let stringConvert = 0; // For function purposes this is a
 let stringConvertTwo = 0; // For function purposes this is b
 let operatorChosen = false; // Used to conditionally check if we are selecting number one or number two
-let operatorSelect = "";
+let operatorSelect = ""; // Used to store what operator the user selected
 
+const numberDisplay = document.querySelector("#current-number");
 const userNumberOne = document.querySelectorAll(".btn-number");
     for (let button of userNumberOne){
         button.addEventListener("click", () => {
@@ -28,9 +29,8 @@ const userOperator = document.querySelectorAll(".btn-operator");
         button.addEventListener("click", () => {
             operatorSelect = button.textContent;
             console.log("The operator selection is:", operatorSelect);
-            numberDisplay.textContent = operatorSelect;
+            numberDisplay.textContent = stringConvert + " " + operatorSelect;
             return operatorChosen = true;
-            return operatorSelect;
         })
     }
 
@@ -40,7 +40,7 @@ const userNumberTwo = document.querySelectorAll(".btn-number");
             if (operatorChosen === true){
                 let numberSelect = button.textContent;
                 secondDisplayNumber = secondDisplayNumber + numberSelect;
-                numberDisplay.textContent = secondDisplayNumber;
+                numberDisplay.textContent = stringConvert + " " + operatorSelect + " " + secondDisplayNumber;
                 stringConvertTwo = parseInt(secondDisplayNumber)
                 console.log("The second number is:", secondDisplayNumber);
         }})
@@ -64,7 +64,6 @@ finalCalculation.addEventListener("click", () => {
             return "Error: Invalid Selection";
         }
     };
-    // Used for testing the function in the console
     let finalResult = operate();
     console.log("The final result is:", finalResult);
     numberDisplay.textContent = finalResult;
