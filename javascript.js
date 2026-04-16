489c89d8f4abeb3ba0a7678badb6e320f1c2c53b
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-16 11:30:19 -0600

    Add in the logic for separating the numbers
    The program now listens and checks conditionally for an operator select.
    If no operator has been selected it keeps collecting the first number.
    Until an operator has been selected then it will check for the
    second number.

--- javascript.js
+++ javascript.js
@@ -4,23 +4,46 @@ const operationMultiply = function (a, b){return a * b;};
 const operationDivide = function (a, b){return a / b;};
 
 const numberDisplay = document.querySelector("#current-number");
-let currentDisplayNumber = "";
+let firstDisplayNumber = ""; // Used to show the current state of the calculator, updates the DOM for number 1
+let secondDisplayNumber = ""; // Used to show the current state of the calculator, updates the DOM for number 2
+let stringConvert = 0; // For function purposes this is a
+let stringConvertTwo = 0; // For function purposes this is b
+let operatorChosen = false;
 
 const userNumberOne = document.querySelectorAll(".btn-number");
     for (let button of userNumberOne){
         button.addEventListener("click", () => {
-            const numberSelect = button.textContent
-            console.log("The first number is:", numberSelect);
-            currentDisplayNumber = currentDisplayNumber + numberSelect;
-            numberDisplay.textContent = currentDisplayNumber;
-        })
-        // parseInt for later
+            if (operatorChosen === false){
+                const numberSelect = button.textContent;
+                firstDisplayNumber = firstDisplayNumber + numberSelect;
+                numberDisplay.textContent = firstDisplayNumber;
+                stringConvert = parseInt(firstDisplayNumber)
+                console.log("The first number is:", firstDisplayNumber);
+        }})
     }
 
-const userOperator = document.querySelectorAll();
+const userOperator = document.querySelectorAll(".btn-operator");
+    for (let button of userOperator){
+        button.addEventListener("click", () => {
+            const operatorSelect = button.textContent;
+            console.log("The operator selection is:", operatorSelect);
+            numberDisplay.textContent = operatorSelect;
+            return operatorChosen = true;
+        })
+    }
 
 
-let userNumberTwo = 4;
+let userNumberTwo = document.querySelectorAll(".btn-number");
+    for (let button of userNumberTwo) {
+        button.addEventListener("click", () => {
+            if (operatorChosen === true){
+                const numberSelect = button.textContent;
+                secondDisplayNumber = secondDisplayNumber + numberSelect;
+                numberDisplay.textContent = secondDisplayNumber;
+                stringConvertTwo = parseInt(secondDisplayNumber)
+                console.log("The first number is:", secondDisplayNumber);
+        }})
+    }
 
 const operate = function (){
     let a = userNumberOne;
